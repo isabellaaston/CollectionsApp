adba24a39d00980fa055ca8507222323e1f362f7
Isabella Aston <isabella.aston8@gmail.com>
2020-03-23 13:49:52 +0000

    add stylesheets

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
 <?php
+?>
+<head>
+<link rel="stylesheet" type="text/css" href="normalize.css">
+<link rel="stylesheet" type="text/css" href="style.css">
+<link href="https://fonts.googleapis.com/css?family=Didact+Gothic&display=swap" rel="stylesheet">
+<title>My Collection</title>
+</head>
+<body>
+    <nav>
+        <div class="menu">
+            <a>Collection</a>
+            <a>Add Item</a>
+            <a>Edit Item</a>
+        </div>
+    </nav>
+<main>
+</main>
+</body>
