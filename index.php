c2f9f5030790e3105cd057a3834b6e35cbef17e7
Isabella Aston <isabella.aston8@gmail.com>
2020-03-24 10:45:06 +0000

    regular commit

--- index.php
+++ index.php
@@ -3,12 +3,11 @@
 <head>
 <link rel="stylesheet" type="text/css" href="normalize.css">
 <link rel="stylesheet" type="text/css" href="style.css">
-<link href="https://fonts.googleapis.com/css?family=Didact+Gothic&display=swap" rel="stylesheet">
 <title>My Collection</title>
 </head>
 <body>
     <nav>
-        <div class="menu">
+        <div class="navbar">
             <a class="underline">Collection</a>
             <a class="underline">Add Item</a>
             <a class="underline">Edit Item</a>
