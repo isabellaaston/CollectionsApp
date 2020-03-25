5e28a42fd405bd7b0e122cc6679a7992f36bebf5
Isabella Aston <isabella.aston8@gmail.com>
2020-03-25 17:15:59 +0000

    getting input into db

--- addBag.php
+++ addBag.php
@@ -1,8 +1,11 @@
+
+<?php
+    require_once ('getDbFunction.php');
+?>
 <html lang=en>
 <head>
         <link rel="stylesheet" type="text/css" href="normalize.css">
         <link rel="stylesheet" type="text/css" href="style.css">
-        <link href="https://fonts.googleapis.com/css?family=Fjalla+One&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Questrial&display=swap" rel="stylesheet">
         <title>My Collection</title>
     </head>
@@ -10,7 +13,7 @@
         <h1>My Handbag Collection</h1>
         <nav>
             <div class="navbar">
-                <a class="underline" href="index.php">Collection</a>
+                <a class="hover" href="index.php">Collection</a>
                 <a class="underline">Add Item</a>
                 <!-- <a class="underline">Edit Item</a> -->
             </div>
@@ -31,14 +34,33 @@
             </div>
             <div>
                 <label>Materials (optional): </label>
-                <input type="text" name ="materials"/><br>
+                <input type="textarea" name ="materials"/><br>
             </div>
             <div>
                 <label>Image Address: </label>
-                <input type="url" name ="image address" required/><br>
+                <input type="url" name ="image" required/><br>
             </div>
-            <input type ="submit" value="Add Bag"/>
+            <input type ="submit" name="submit" value="Add Bag"/>
         </form>
+        <?php
+
+        $db =getDb();
+
+        if (isset($_POST['submit']))
+        {
+            $brand = $_POST['brand'];
+            $model = $_POST['model'];
+            $price = intval($_POST['price']);
+            $materials = $_POST['materials'];
+            $image = $_POST['image'];
+
+            $query=$db->prepare ("INSERT INTO `Bags` (`brand`, `image`, `model`, `price`, `materials`)VALUES($brand, $image, $model, $price, $materials);");
+            var_dump ($price);
+            $query -> execute();
+            echo "Bag Added";
+        }
+
+        ?>
     </main>
     </body>
 </html>
